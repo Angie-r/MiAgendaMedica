8b309fd4a56f3d67c50dc52c43dd7ba18ae2ba8d
Angela Romero <anjera27@gmail.com>
2022-08-09 12:19:46 -0600

    Se agrega el objeto Doctor y paciente.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import javax.print.Doc;
+
 import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        showMenu();
+        //showMenu();
+
+        Doctor myDoctor = new Doctor("Gabriela Romero", "Cirugia");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.especialidad);
+
+        Paciente myPaciente = new Paciente("Angela Silva", "anjera27@gmail.com");
+        System.out.println(myPaciente.name);
+        System.out.println(myPaciente.email);
     }
 
 
