214aec7c3e77e4a3c4989aafabb9db3a35eb00bf
Angela Romero <anjera27@gmail.com>
2022-08-09 18:38:32 -0600

    Se realiza el encapsulamiento de la clase y se generan los metodos getter y setter a las variables, se condiciona phoneNumber para que solo acepte 8 digitos.

--- src/Paciente.java
+++ src/Paciente.java
@@ -1,23 +1,97 @@
 public class Paciente {
     public static int id = 0;
-
-    String name;
-    String email;
-    String address;
-    String phoneNumber;
-    String birthday;
-    double weight; //peso
-    double heigth; //altura
+    //Atributos
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String birthday;
+    private double weight; //peso
+    private double heigth; //altura
     String blood;
 
-
     Paciente(String name, String email){
-        System.out.println("\n");
-        System.out.println("El nombre del paciente es: "+ name);
+        //System.out.println("\n");
+        //System.out.println("El nombre del paciente es: "+ name);
         this.name = name;
         this.email = email;
+    }
+
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
+    // 54.5 Kg. String
+    public String getWeight(){
+        return this.weight + " Kg. ";
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        Paciente.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (phoneNumber.length() > 8){
+            System.out.println("El numero de telefono debe ser de 8 digitos");
+        } else if (phoneNumber.length()==8) {
+            this.phoneNumber = phoneNumber;
+            System.out.println(phoneNumber);
+        }
 
     }
 
+    public String getBirthday() {
+        return birthday;
+    }
 
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getHeigth() {
+        return heigth + " Mts.";
+    }
+
+    public void setHeigth(double heigth) {
+        this.heigth = heigth;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
 }
